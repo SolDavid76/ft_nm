72312019ea9153b4f11165f1c382fd56a70ec09b
SolDavid_76 <david.janusz75@gmail.com>
2024-09-26 16:19:31 +0200

    almost done

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: djanusz   <djanusz  @student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 21:21:43 by djanusz           #+#    #+#             */
-/*   Updated: 2024/09/24 15:23:55 by djanusz          ###   ########.fr       */
+/*   Updated: 2024/09/26 15:44:59 by djanusz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,12 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <elf.h>
+
+#define NO_SORT      1  // 00001 = -p
+#define REVERSE_SORT 2  // 00010 = -r
+#define PRINT_ALL    4  // 00100 = -a
+#define PRINT_EXTERN 8  // 01000 = -g
+#define PRINT_UNDEF  16 // 10000 = -u
 
 typedef struct s_symbol
 {
@@ -107,6 +113,34 @@ void ft_swap(t_symbol *ptr1, t_symbol *ptr2)
 	*ptr2 = tmp;
 }
 
+char *ft_strdup(char *str)
+{
+	int   i = 0;
+	char *res = malloc(sizeof(char) * (ft_strlen(str) + 1));
+
+	while (str[i])
+	{
+		res[i] = str[i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
+
+char *minimalize(char *str)
+{
+	char *res = malloc(sizeof(char) * ft_strlen((str) + 1));
+
+	for (int i = 0; str[i]; i++)
+	{
+		if ('A' <= str[i] && str[i] <= 'Z')
+			res[i] = str[i] + 32;
+		else
+			res[i] = str[i];
+	}
+	return (res);
+}
+
 int ft_strcmp(const char *s1, const char *s2)
 {
     int	i = 0, j = 0;
@@ -141,7 +175,9 @@ t_symbol *handle_64_symbols_sort(t_symbol *tab, int tabSize)
 			while (tab[j].name[l] && tab[j].name[l] == '_')
 				l++;
 
-			if (ft_strcmp(tab[i].name, tab[j].name) == 0 && tab[i].adress > tab[j].adress)
+			if (ft_strcmp(tab[i].name, tab[j].name) == 0 && tab[i].type == tab[j].type && tab[i].adress > tab[j].adress)
+				ft_swap(&tab[i], &tab[j]);
+			else if (ft_strcmp(tab[i].name, tab[j].name) == 0 && tab[i].type < tab[j].type)
 				ft_swap(&tab[i], &tab[j]);
 			else if (ft_strcmp(tab[i].name_min + k, tab[j].name_min + l) == 0 && k < l)
 				ft_swap(&tab[i], &tab[j]);
@@ -200,52 +236,71 @@ void ft_putnbr_base(unsigned int nb, char *base)
 	}
 }
 
+void print_64_symbol(t_symbol *symbol)
+{
+	if (symbol->type != 'U' && (symbol->adress != 0 || symbol->type == 'a'))
+	{
+		for (int j = hexalen(symbol->adress); j < 16; j++)
+			write(1, "0", 1);
+		ft_putnbr_base(symbol->adress, "0123456789abcdef");
+	}
+	else
+		write(1, "                ", 16);
+	write(1, " ", 1);
+	write(1, &symbol->type, 1);
+	write(1, " ", 1);
+	write(1, symbol->name, ft_strlen(symbol->name));
+	write(1, "\n", 1);
+}
+
 void print_64_symbols(t_symbol *tab, int tabSize)
 {
-	for (int i = 0; i < tabSize - 1; i++)
+	if (!(flags & REVERSE_SORT))
 	{
-		if (tab[i].type != 'U' && tab[i].type != 'w')
+		for (int i = 0; i < tabSize - 1; i++)
 		{
-			for (int j = hexalen(tab[i].adress); j < 16; j++)
-				write(1, "0", 1);
-			ft_putnbr_base(tab[i].adress, "0123456789abcdef");
+			if (flags & PRINT_ALL)
+				print_64_symbol(&tab[i]);
+			else if (flags & PRINT_EXTERN)
+			{
+				if (('A' <= tab[i].type && tab[i].type <= 'Z') || tab[i].type == 'u' || tab[i].type == 'v' || tab[i].type == 'w')
+					print_64_symbol(&tab[i]);
+			}
+			else if (flags & PRINT_UNDEF)
+			{
+				if (tab[i].type == 'U' || tab[i].type == 'v' || tab[i].type == 'w')
+					print_64_symbol(&tab[i]);
+			}
+			else
+			{
+				if (tab[i].type != 'a')
+					print_64_symbol(&tab[i]);
+			}
 		}
-		else
-			write(1, "                ", 16);
-		write(1, " ", 1);
-		write(1, &tab[i].type, 1);
-		write(1, " ", 1);
-		write(1, tab[i].name, ft_strlen(tab[i].name));
-		write(1, "\n", 1);
 	}
-}
-
-char *minimalize(char *str)
-{
-	char *res = malloc(sizeof(char) * ft_strlen((str) + 1));
-
-	for (int i = 0; str[i]; i++)
+	else
 	{
-		if ('A' <= str[i] && str[i] <= 'Z')
-			res[i] = str[i] + 32;
-		else
-			res[i] = str[i];
+		for (int i = tabSize - 1 ; i > 0; i--)
+		{
+			if (flags & PRINT_ALL)
+				print_64_symbol(&tab[i]);
+			else if (flags & PRINT_EXTERN)
+			{
+				if (('A' <= tab[i].type && tab[i].type <= 'Z') || tab[i].type == 'u' || tab[i].type == 'v' || tab[i].type == 'w')
+					print_64_symbol(&tab[i]);
+			}
+			else if (flags & PRINT_UNDEF)
+			{
+				if (tab[i].type == 'U' || tab[i].type == 'v' || tab[i].type == 'w')
+					print_64_symbol(&tab[i]);
+			}
+			else
+			{
+				if (tab[i].type != 'a')
+					print_64_symbol(&tab[i]);
+			}
+		}
 	}
-	return (res);
-}
-
-char *ft_strdup(char *str)
-{
-	int   i = 0;
-	char *res = malloc(sizeof(char) * (ft_strlen(str) + 1));
-
-	while (str[i])
-	{
-		res[i] = str[i];
-		i++;
-	}
-	res[i] = '\0';
-	return (res);
 }
 
 void handle_64_symbols(char *ptr, Elf64_Ehdr *header, Elf64_Shdr *sections, Elf64_Shdr *symTab)
@@ -264,9 +319,13 @@ void handle_64_symbols(char *ptr, Elf64_Ehdr *header, Elf64_Shdr *sections, Elf6
 		tab[i - 1].name_min = minimalize(tab[i - 1].name);
 	}
 
-	handle_64_symbols_sort(tab, symTabSize);
+	if (!(flags & NO_SORT))
+		handle_64_symbols_sort(tab, symTabSize);
 
 	print_64_symbols(tab, symTabSize);
+
+	//free tab here
+	exit(0);
 }
 
 void handle_64(char *ptr)
@@ -279,7 +338,6 @@ void handle_64(char *ptr)
 		if (sections[i].sh_type == SHT_SYMTAB)
 		{
 			handle_64_symbols(ptr, header, sections, sections + i);
-			break ;
 		}
 	}
 }
@@ -294,13 +352,64 @@ void nm(char *ptr)
 		printf("Is not an ELF\n");
 }
 
+int ft_nbFiles(char **tab)
+{
+	int res = 0;
+
+	for (int i = 0; tab[i]; i++)
+	{
+		if (tab[i][0] != '-')
+			res++;
+	}
+	return (res);
+}
+
+void parse_flag(char **tab)
+{
+	for (int i = 0; tab[i]; i++)
+	{
+		for (int j = 1; tab[i][0] == '-' && tab[i][j]; j++)
+		{
+			if (tab[i][j] == 'p')
+				flags |= NO_SORT;
+			else if (tab[i][j] == 'r')
+				flags |= REVERSE_SORT;
+			else if (tab[i][j] == 'a')
+				flags |= PRINT_ALL;
+			else if (tab[i][j] == 'g')
+				flags |= PRINT_EXTERN;
+			else if (tab[i][j] == 'u')
+				flags |= PRINT_UNDEF;
+			else
+			{
+				write(2, "ft_nm: invalid option -- '", 26);
+				write(2, &tab[i][j], 1);
+				write(2, "'\n", 2);
+				write(2, "Usage: ft_nm [option(s)] [file(s)]\n", 35);
+				write(2, "List symbols in [file(s)] (a.out by default).\n", 46);
+				write(2, "The options are:\n", 18);
+				write(2, "-a,                    Display debugger-only symbols\n", 53);
+				write(2, "-g,                    Display only external symbols\n", 53);
+				write(2, "-p,                    Do not sort the symbols\n", 47);
+				write(2, "-r,                    Reverse the sense of the sort\n", 53);
+				write(2, "-u,                    Display only undefined symbols\n", 54);
+				exit(1);
+			}
+		}
+	}
+}
+
 int main(int ac, char **av)
 {
 	int         fd;
 	char       *ptr;
 	struct stat buf;
 
-	if (ac == 1)
+	int         nbFiles = ft_nbFiles(av + 1);
+
+	parse_flag(av + 1);
+
+	if (nbFiles == 0)
 		main(ac + 1, (char *[]){av[0], "a.out", NULL});
 	else
 	{
@@ -308,7 +417,7 @@ int main(int ac, char **av)
 		{
 			if (av[i][0] != '-')
 			{
-				if ((fd = open(av[1], O_RDONLY)) < 0)
+				if ((fd = open(av[i], O_RDONLY)) < 0)
 					printf("Can't open file\n");
 				else if (fstat(fd, &buf) < 0)
 					printf("Can't read the file\n");
@@ -324,9 +433,3 @@ int main(int ac, char **av)
 	}
 	return (0);
 }
-
-// 00001 = -p (no sort)
-// 00010 = -r (reverse sort)
-// 00100 = -a (all)
-// 01000 = -g (extern only)
-// 10000 = -u (undefined only)
